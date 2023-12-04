40378e94c917818923533816f2ac844ab33185fd
LucasSarri <lucassarri@hotmail.com>
2023-12-04 15:11:14 -0400

    feat(tipos de dados): trabalhando com números inteiros

---
+++ tipos de dados/tipoNumber.js
@@ -0,0 +1,13 @@
+// Números inteiros
+const numero = 3;
+
+const num1 = 1;
+const num2 = 2;
+const soma = num1+num2;
+
+console.log(soma);
+
+// Números decimais ou ponto flutuante
+const num3 = 3.3
+const num4 = .5;
+const multi = num3*num4;
